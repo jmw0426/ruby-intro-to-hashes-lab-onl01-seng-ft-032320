d9de5f728d80c6d108091ccf6628364a7ac4d3f3
Joshua Williams <jmw0426@hotmail.com>
2020-03-30 22:59:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ end
 def monopoly_with_second_tier
 		monopoly =  {
   railroads: {
-    pieces: 4, names:{}, rent_in_dollars:{one_piece_owned: 25,} 
+    pieces: 4, names:{}, rent_in_dollars:{one_piece_owned: 25, two_pieces_owned: 50, } 
   }
 	}
 
